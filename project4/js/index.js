ebb8d8c4ea84d103bcf57de9d97cfc51f20ab109
uai <noema01@outlook.jp>
2020-09-02 17:41:15 +0900

    comp project4

--- project4/js/index.js
+++ project4/js/index.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('#message-form');
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    const message = document.querySelector('#message');
+    const feedback = document.querySelector('.note-text');
+    const messageContent = document.querySelector('.change-text');
+
+    if (message.value === '') {
+        feedback.classList.remove('el-hidden');
+        setTimeout(function () {
+        feedback.classList.add('el-hidden');
+        }, 2000);
+    } else {
+        messageContent.textContent = message.value;
+        message.value = '';
+    }
+})
